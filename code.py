4755059e1f4a9b5e46056baa6d4fa2443bcc2b43
venkat8306 <svkmarvelous@gmail.com>
2024-10-25 16:23:00 +0530

    "Add comments to explain function"

--- code.py
+++ code.py
@@ -887,3 +887,10 @@ class TestconversationalAI-saraFunctions(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+import unittest
+class TestconversationalAI-saraFunctions(unittest.TestCase):
+    def test_example_function(self):
+        self.assertEqual(example_function(), expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
